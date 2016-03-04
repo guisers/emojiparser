8bc9af51373c51e5da92d1e25e2ff313371a1591
Crystal To <crystal.kyto@gmail.com>
2016-03-03 23:30:56 -0800

    Add reference box while typing emoji shortcodes

--- content_script.js
+++ content_script.js
@@ -13,11 +13,37 @@ function watchAll(e) {
 	if (!fullInput) return; 
 	inputBeforeCursor = fullInput.slice(0, cursor);
 
-	if (regex.test(inputBeforeCursor)) {
+	var tooltip = document.getElementById("emojiParserTooltip");
+	if (!tooltip) { // create tooltip if haven't yet
+		tooltip = document.createElement('div');
+		tooltip.id = "emojiParserTooltip";
+		tooltip.style.cssText = "position: fixed;bottom: 0px;right: 0px;z-index: 1000;white-space: pre;background-color: rgba(255, 255, 255, 0.9);font-family: 'Gill Sans MT', Arial;font-size: 16px;color: #303030;padding: 10px;border: solid 1px #aaa;max-height:350px;overflow:scroll;";
+		document.body.appendChild(tooltip);
+	}
+
+	tooltip.textContent = "";
+	tooltip.style.display = "none";
+
+	if (regex.test(inputBeforeCursor)) {	
+		var match = inputBeforeCursor.match(regex)[0];
+		var emoji = match.slice(match.indexOf(":"), match.length);
+
+		var suggestions = emojiKeys.filter(function(value){
+			return value.indexOf(emoji) >= 0;
+		});
+		
+		for (var i=0; i<suggestions.length;i++) {
+			tooltip.textContent += emojiMap[suggestions[i]]+"   "+suggestions[i];
+			if (i<suggestions.length-1) {
+				tooltip.textContent += "\r\n\r\n";
+			}
+		}
+		if (suggestions.length>0) {
+			tooltip.style.display = "block";
+		}
+		
 		var lastCharacter = inputBeforeCursor.slice(inputBeforeCursor.length-1);
 		if (lastCharacter == ":") {
-			var match = inputBeforeCursor.match(regex)[0];
-			var emoji = match.slice(match.indexOf(":"), match.length);
 			var unicode = emojiMap[emoji];
 			if (unicode) {
 				var toTruncate = inputBeforeCursor.length - emoji.length;
@@ -36,6 +62,8 @@ function watchAll(e) {
 				  selection.removeAllRanges();
 				  selection.addRange(range);
 				}
+				tooltip.textContent = "";
+				tooltip.style.display = "none";
 			}
 		}
 	}
@@ -891,3 +919,5 @@ var emojiMap = {
    ":yum:":"\u{1F60B}",
    ":zap:":"\u{026A1}"
 }
+
+var emojiKeys = Object.keys(emojiMap);
